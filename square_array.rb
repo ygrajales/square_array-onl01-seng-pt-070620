b0a45de5ee2b61586fe799fabac97352a0f39fa9
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-13 21:02:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
-def square_array(array)
-  # your code here
+# def square_array(array)
+#   # your code here
   
-  array_2 = []
+#   array_2 = []
   
-  array.each do |number|
-    array_2 << number**2
-  end
+#   array.each do |number|
+#     array_2 << number**2
+#   end
   
-  array_2
+#   array_2
   
-end
+# end
