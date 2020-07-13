b729c7acb16b0a57a527ba7121036fb9b55294f5
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-13 21:10:21 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
   # your code here
+  
+  array_2 = []
+  
+  array.each do |number|
+    array_2 << number**2
+  end
+  
+  array_2
+  
+end
+
+def square_higher_level(array)
+  array.collect{|number| number*number}
 end
